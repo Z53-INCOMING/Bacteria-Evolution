8fa6f307314fd08e84b1949e93e4b78428528168
Z53-INCOMING <75651517+Z53-INCOMING@users.noreply.github.com>
2022-04-29 13:21:16 -0700

    More tutorial.

--- Tutorial.gd
+++ Tutorial.gd
@@ -5,4 +5,13 @@ onready var camera = $Camera2D
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
 		camera.position.y += 180
-	
+	if Input.is_action_just_pressed("ui_down"):
+		camera.position.y += 180
+	if Input.is_action_just_pressed("ui_up"):
+		camera.position.y -= 180
+	if camera.position.y > (20 * 180):
+		camera.position.y = 20 * 180
+
+
+func _on_Button_button_down() -> void:
+	get_tree().change_scene("res://Menu.tscn")
